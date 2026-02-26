47652ce09e77b017e66563e1a4c41162f0bc6dde
Devayani <devayaniit@gmail.com>
2026-02-26 11:26:19 +0530

    Commited Successfully

---
+++ src/org/test/HelloWorld.java
@@ -0,0 +1,11 @@
+package org.test;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		String s = "Hello World";
+		String s1 = s.toUpperCase();
+		System.out.println("String:" +s1);
+	}
+
+}
